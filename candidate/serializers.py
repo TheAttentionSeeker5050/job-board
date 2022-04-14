1fa3519b6b83f4dc50b3fdb7cf3d816220c30a5d
Nicolas Castellano <nicolas.castellano.6@gmail.com>
2022-04-14 16:37:33 -0500

    fixed some stuff, made a basic api viewset for the customer create

--- candidate/serializers.py
+++ candidate/serializers.py
@@ -9,12 +9,17 @@ from candidate.models import Candidate
 from django.contrib.auth.hashers import make_password
 from rest_framework.validators import UniqueValidator
 
-class CandidateSerializer(serializers.HyperlinkedModelSerializer):
+class CreateCandidateProfileSerializer(serializers.ModelSerializer):
     """ 
         With this serializer we interact with our rest api at an user (candidate) level
         we added some built-in hashing and password validators
         I am using a custom model view because I am making a custom user (candidate) model
      """
+
+    class Meta:
+        model = Candidate
+        fields = ("pk","email", "username", "password", "first_names", "last_name")
+
     email = serializers.EmailField(
         validators=[UniqueValidator(Candidate.objects.all())]
         )
@@ -31,15 +36,17 @@ class CandidateSerializer(serializers.HyperlinkedModelSerializer):
         style = {"input_type": "password", "placeholder": "Password"}
     )
 
-    class Meta:
-        model = Candidate,
-        fields = ("email", "username", "password", "first_names", "last_name", "city_of_residence", "state_province", "country")
 
     def create(self, validated_data):
-        fields = ["email", "username", "first_names", "last_name", "city_of_residence", "state_province", "country"]
-        validated_data = {f: validated_data.get(f) for f in fields}
-        validated_data["password"] = make_password(validated_data("password"))
-        return super(CandidateSerializer, self).create(validated_data)
+        validated_data["password"] = make_password(validated_data.get("password"))
+        candidate = super().create(validated_data)
+        candidate.save()
+        return candidate
+        # fields = ["email", "username", "first_names", "last_name"]
+        # validated_data = {f: validated_data.get(f) for f in fields}
+        # validated_data["password"] = make_password(validated_data("password"))
+        # return super(CreateCandidateProfileSerializer, self).create(validated_data)
+
 
     def update(self, instance, validated_data):
         candidate = super().update(instance, validated_data)
@@ -49,3 +56,4 @@ class CandidateSerializer(serializers.HyperlinkedModelSerializer):
         except KeyError:
             pass
         return candidate
+
--- candidate/views.py
+++ candidate/views.py
@@ -9,7 +9,7 @@ from rest_framework.viewsets import ModelViewSet
 
 
 # import serializers
-from candidate.serializers import CandidateSerializer
+from candidate.serializers import CreateCandidateProfileSerializer
 
 # some request and response libs
 from django.http import Http404
@@ -24,40 +24,42 @@ class CandidateCustomView(ModelViewSet):
         Creates, deletes and updates user instances, retrieves this is a custom view
     """
     queryset = Candidate.objects.all()
-    serializer_class = CandidateSerializer
+    serializer_class = CreateCandidateProfileSerializer
 
-    def get_object(self, pk):
-        try:
-            return Candidate.objects.get(pk=pk)
-        except Candidate.DoesNotExist:
-            raise Http404
-    @action(detail=True, methods=["get"])
-    def get(self, request, pk, format=None):
-        # for retrieving candidate information
-        candidate = self.get_object(pk)
-        serializer = CandidateSerializer(candidate)
-        return Response(serializer.data)
+    # def get_object(self, pk):
+    #     try:
+    #         return Candidate.objects.get(pk=pk)
+    #     except Candidate.DoesNotExist:
+    #         raise Http404
+    # # @action(detail=True, methods=["get"])
+    # def get(self, request, pk, format=None):
+    #     # for retrieving candidate information
+    #     candidate = self.get_object(pk)
+    #     serializer = CreateCandidateProfileSerializer(candidate)
+    #     return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        # for updating candidate information
-        candidate = self.get_object(pk)
-        serializer = CandidateSerializer(candidate, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request, pk, format=None):
+    #     # for updating candidate information
+    #     candidate = self.get_object(pk)
+    #     serializer = CreateCandidateProfileSerializer(candidate, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        # for deleting a candidate
-        candidate = self.get_object(pk)
-        candidate.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # def delete(self, request, pk, format=None):
+    #     # for deleting a candidate
+    #     candidate = self.get_object(pk)
+    #     candidate.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(detail=True, methods=["post"])
-    def post(self, request, format=None):
-        # create a new candidate profile
-        serializer = CandidateSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # # @action(detail=True, methods=["post"])
+    # def post(self, request, format=None):
+    #     # create a new candidate profile
+    #     serializer = CreateCandidateProfileSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
